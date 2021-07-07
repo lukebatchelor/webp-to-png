d468fcfe73bda573566e8ad2aa7144c968261be0
Luke.Batchelor <batchelor.luke+git@gmail.com>
2021-07-07 21:56:08 +1000

    Adds workbox to add service workers for pwa support

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,33 +1,40 @@
-import esbuild from "esbuild";
-import serve, { error, log } from "create-serve";
+import esbuild from 'esbuild';
+import serve, { error, log } from 'create-serve';
+import { generateSW } from 'workbox-build';
 
-const isDevServer = process.argv.includes("--dev");
+const isDevServer = process.argv.includes('--dev');
 
 esbuild
   .build({
-    entryPoints: ["src/index.tsx"],
+    entryPoints: ['src/index.tsx'],
     bundle: true,
-    outfile: "www/dist/bundle.js",
+    outfile: 'www/dist/bundle.js',
     minify: !isDevServer,
     sourcemap: true,
     incremental: isDevServer,
-    target: ["chrome58", "firefox57", "safari11", "edge18"],
+    target: ['chrome58', 'firefox57', 'safari11', 'edge18'],
     define: {
-      "process.env.NODE_ENV": isDevServer ? '"development"' : '"production"',
+      'process.env.NODE_ENV': isDevServer ? '"development"' : '"production"',
     },
     watch: isDevServer && {
       onRebuild(err) {
         serve.update();
-        err ? error("❌ Failed") : log("✅ Updated");
+        err ? error('❌ Failed') : log('✅ Updated');
       },
     },
   })
   .catch(() => process.exit(1));
 
+generateSW({
+  swDest: './www/sw.js',
+  globDirectory: './www',
+  globPatterns: ['dist/*.{css,js}', '*.{png,ico,html,.webmanifest}'],
+});
+
 if (isDevServer) {
   serve.start({
     port: 5000,
-    root: "./www",
+    root: './www',
     live: true,
   });
 }
