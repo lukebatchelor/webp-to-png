2ffcc51d176e9031dc9941b138f3587dd381d250
Luke.Batchelor <batchelor.luke+git@gmail.com>
2021-07-18 18:27:56 +1000

    Adding target=_blank and noopener noreferrer rel's

--- src/App.tsx
+++ src/App.tsx
@@ -125,7 +125,7 @@ export function App() {
           </Button>
         )}
       </Box>
-      <a ref={downloadAnchorRef} className={classes.hidden} />
+      <a ref={downloadAnchorRef} className={classes.hidden} target="_blank" rel="noopener noreferrer" />
     </Container>
   );
 }
